f2902a7805e578929bfd4686897be2dd2daf37cc
DJAYI <danilo.arenasyi@gmail.com>
2026-04-21 17:00:47 -0500

    feat(dtos): add request and response DTOs for parametrization types

---
+++ backend/geopix/geopix/src/main/java/com/agmdesarrollos/geopix/features/predios/http/dtos/PropertyTypeResponse.java
@@ -0,0 +1,16 @@
+package com.agmdesarrollos.geopix.features.predios.http.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PropertyTypeResponse {
+    private Long id;
+    private String name;
+    private String description;
+}
